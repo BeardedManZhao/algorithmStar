f7a9dd135a447bb0341fa348bd590d75d3090112
LingYuZhao <liming7887@qq.com>
2022-10-31 19:18:47 +0800

    Update MAIN1.java

--- src_code/src/main/java/zhao/algorithmMagic/MAIN1.java
+++ src_code/src/main/java/zhao/algorithmMagic/MAIN1.java
@@ -4,7 +4,10 @@ import zhao.algorithmMagic.integrator.HashClassificationIntegrator;
 import zhao.algorithmMagic.integrator.launcher.HashClassificationLauncher;
 import zhao.algorithmMagic.utils.dataContainer.SetAndValue;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class MAIN1 {
     public static void main(String[] args) {
@@ -12,11 +15,9 @@ public class MAIN1 {
         SetAndValue<String> stringSetAndValue1 = new SetAndValue<>("兔子", "会动", "有毛");
         SetAndValue<String> stringSetAndValue2 = new SetAndValue<>("仓鼠", "会动", "有毛");
         SetAndValue<String> stringSetAndValue3 = new SetAndValue<>("赵", "会动", "有毛", "会玩手机");
-        SetAndValue<String> stringSetAndValue4 = new SetAndValue<>("杨", "会动", "有毛", "会玩手机", "是高中生");
         // 定义三个类别
         HashMap<String, HashSet<String>> hashMap = new HashMap<>();
         hashMap.put("人类", new HashSet<>(Arrays.asList("会动", "有毛", "会玩手机")));
-        hashMap.put("奴隶", new HashSet<>(Arrays.asList("有毛", "会玩手机", "是高中生")));
         hashMap.put("动物", new HashSet<>(Arrays.asList("会动", "有毛")));
 
         // 实现分类启动器
@@ -41,7 +42,7 @@ public class MAIN1 {
             // 配置需要被分类的数据集
             @Override
             public List<SetAndValue<String>> CategorizedData() {
-                return Arrays.asList(stringSetAndValue1, stringSetAndValue2, stringSetAndValue3, stringSetAndValue4);
+                return Arrays.asList(stringSetAndValue1, stringSetAndValue2, stringSetAndValue3);
             }
         };
 
